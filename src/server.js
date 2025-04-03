a3dcd916d423d3df947f6676e65ab0a7dcad629b
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2025-04-03 17:55:27 +0200

    server webSocket

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const { Server } = require('socket.io');
+const config = JSON.parse(fs.readFileSync("config.json"));
+const userList = []; 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+
+//Creazione del Server WebSocket
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+server.listen(config.port, () => {
+    console.log("server running on port: " + config.port);
+});
+
+io.on('connection', (socket) => {
+    console.log("socket connected: " + socket.id);
+    //io.emit("chat", "new client: " + socket.id);
+    socket.on('message', (message) => {
+        const response = (userList.find( user => user.socketID == socket.id).name) + ': ' + message;
+        console.log(response);
+        io.emit("chat", response);
+    });
+    socket.on("isLogged", (username) => {
+        userList.push({ socketID: socket.id, name: username });
+        io.emit("list", userList);
+        console.log(userList)
+     })
+     socket.on("disconnect", () => {
+        userList = userList.filter( user => user.socketID != socket.id)
+        console.log(userList)
+        console.log("socket disconnected: " + socket.id);
+     })
+});
+
+
